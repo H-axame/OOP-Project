d976ea236dd454eaddf9deb76217ff87c3cbabb1
H-axame <68094943+H-axame@users.noreply.github.com>
2022-05-15 14:16:47 +0800

    3 class aku dh buat cuma main.java je blum

---
+++ src/School/Management/System/School.java
@@ -0,0 +1,57 @@
+package School.Management.System;
+
+import java.util.List;
+
+public class School {
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+
+    public School (List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned=0;
+        totalMoneySpent=0;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    //add a teacher to the school
+    public void addTeachers(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    //list of student in school
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    //add a student to the school
+    public void addStudents(Student student) {
+        students.add(student);
+    }
+
+    //return the total money earned by the school
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    //add the total earned by the school
+    public void updateTotalMoneyEarned(int MoneyEarned) {
+        totalMoneyEarned += MoneyEarned;
+    }
+
+    //return the total money spent by the school
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    //update the money that is spent by the school which is the salary given to its teachers
+    public void updateTotalMoneySpent(int MoneySpent) {
+        totalMoneyEarned -= MoneySpent;
+    }
+}
